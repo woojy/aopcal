b62166249879b1f7232a56310baf79b745e3c083
Jaeyun Woo <woojy3770@e-mirim.hs.kr>
2018-06-15 10:13:32 +0900

    aopcal 프로젝트 사전작업

---
+++ src/main/java/kr/hs/emirim/woojy/aopcal/cal/Calculator.java
@@ -0,0 +1,7 @@
+package kr.hs.emirim.woojy.aopcal.cal;
+
+public interface Calculator {
+
+	public long factorial(long num);
+
+}
---
+++ src/main/java/kr/hs/emirim/woojy/aopcal/cal/ImpeCalculator.java
@@ -0,0 +1,17 @@
+package kr.hs.emirim.woojy.aopcal.cal;
+public class ImpeCalculator implements Calculator {
+
+	@Override
+	public long factorial(long num) {
+		long start = System.currentTimeMillis();
+		
+		long result = 1;
+		for (long i = 1; i <= num; i++) {
+			result *= i;
+		}
+		
+		long end = System.currentTimeMillis();
+		System.out.printf("Imp factorial (%d): 실행시간 = %d",num,(end-start));
+		return result;
+	}
+}
