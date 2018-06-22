85fe269f6983b04c56d81bd775c41daf5b162642
Jaeyun Woo <woojy3770@e-mirim.hs.kr>
2018-06-22 09:47:04 +0900

    @Aspect 애노테이션 기법으로 AOP구현

---
+++ src/main/java/kr/hs/emirim/woojy/aopcal/main/MainXmlAspect.java
@@ -0,0 +1,23 @@
+package kr.hs.emirim.woojy.aopcal.main;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import kr.hs.emirim.woojy.aopcal.cal.Calculator;
+
+public class MainXmlAspect {
+	public static void main(String[] args) {
+		GenericXmlApplicationContext ctx =
+				new GenericXmlApplicationContext("classpath:aopAspect.xml");
+		
+		Calculator impCal = ctx.getBean("impCal",Calculator.class);
+		
+		long fiveFact1 = impCal.factorial(5);
+		System.out.println("impCal.factorical(5) = "+fiveFact1);
+		
+		
+		Calculator recCal =  ctx.getBean("recCal",Calculator.class);
+		long fiveFact2 = recCal.factorial(5);
+		System.out.println("recCal.factorial(5) = "+fiveFact2);
+	}
+
+}
